73a978f75104281943e60627d9ba911128f260ad
Clayton Sweeten <cjsweeten101@gmail.com>
2024-11-23 14:13:38 -0700

    computer responds to guess by returning a hash

--- lib/computer.rb
+++ lib/computer.rb
@@ -7,6 +7,31 @@ class Computer
   end
 
   def generate_code
-    Array.new(4) { rand(6) }
+    Array.new(4) { rand(1..6) }
+  end
+
+  def respond_to_guess(guess)
+    result = { exact: 0, wrong_spot: 0 }
+    code_copy = @random_code.clone
+    guess.each_with_index do |num, index|
+      if code_copy[index] == num
+        result[:exact] += 1
+        code_copy[index] = nil
+      elsif code_copy.include? num
+        idx = code_copy.find_index { |val| val == num }
+        check_for_future_matches(idx, code_copy, guess, result)
+      end
+    end
+    result
+  end
+
+  def check_for_future_matches(idx, code, guess, result)
+    if code[idx] == guess[idx]
+      p 'here?'
+      result[:exact] += 1
+    else
+      result[:wrong_spot] += 1
+    end
+    code[idx] = nil
   end
 end
